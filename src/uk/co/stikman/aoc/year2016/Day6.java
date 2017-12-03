165771b49fc9a13448e50a5778bfa89d59b5f905
Stik <stikdragon@gmail.com>
2017-12-03 12:26:23 +0000

    2016/day6

--- src/uk/co/stikman/aoc/year2016/Day6.java
+++ src/uk/co/stikman/aoc/year2016/Day6.java
@@ -1,5 +1,12 @@
 package uk.co.stikman.aoc.year2016;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import uk.co.stikman.aoc.utils.AoCBase;
 import uk.co.stikman.aoc.utils.ConsoleOutput;
 import uk.co.stikman.aoc.utils.Output;
@@ -12,10 +19,33 @@ public class Day6 extends AoCBase {
 
 	@Override
 	public void run(String input, int part, Output out) {
-		if (part == 0) {
-		} else if (part == 1) {
-		} else
-			throw new RuntimeException("Part.. ?");
+		List<String> lines = Arrays.stream(input.split("\n")).map(String::trim).collect(Collectors.toList());
+			int size = lines.stream().map(String::length).mapToInt(n -> n).max().orElse(0);
+			List<Map<Character, Integer>> counts = new ArrayList<>();
+			for (int i = 0; i < size; ++i)
+				counts.add(new HashMap<>());
+			for (String line : lines) {
+				for (int i = 0; i < size; ++i) {
+					Map<Character, Integer> map = counts.get(i);
+					char ch = line.charAt(i);
+					Integer cnt = map.get(ch);
+					if (cnt == null)
+						map.put(ch, 1);
+					else
+						map.put(ch, cnt + 1);
+				}
+			}
+
+			char[] res = new char[size];
+			for (int i = 0; i < size; ++i) {
+				Map<Character, Integer> map = counts.get(i);
+				if (part == 0)
+					res[i] = map.entrySet().stream().sorted((a, b) -> b.getValue() - a.getValue()).findFirst().orElseThrow(RuntimeException::new).getKey();
+				else if (part == 1)
+					res[i] = map.entrySet().stream().sorted((a, b) -> a.getValue() - b.getValue()).findFirst().orElseThrow(RuntimeException::new).getKey();
+			}
+			out.println("Recovered password is: " + new String(res));
+
 
 	}
 
