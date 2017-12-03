71426446e5c6cdf60b75c190696344b7b2d2916d
Stik <stikdragon@gmail.com>
2017-12-03 16:21:21 +0000

    failed attempt at 2016/11, done 2016/12

--- src/uk/co/stikman/aoc/year2016/Day11.java
+++ src/uk/co/stikman/aoc/year2016/Day11.java
@@ -10,6 +10,37 @@ public class Day11 extends AoCBase {
 		new Day11().run(SourceData.get(11), 1, new ConsoleOutput());
 	}
 
+	/**
+	 * 
+	 * <pre>
+	4    -    -    -    -    -    -    -    -    -    -  
+	3    -    -    -    -    -    -    -    -    -    -
+	2    -   PoM   -   PrM   -    -    -    -    -    -  
+	1   PoG   -   PrG   -   ThG  ThM  RuG  RuM  CoG  CoM
+	 * </pre>
+	 * 
+	 **/
+
+	//
+	// This seems like a human puzzle rather than a computing one.. ?
+	//  Step 1 - E up with PoG and ThG
+	//  Step 2 - E down 
+	//  Step 3 - E up with PrG and RuG
+	//  Step 4 - E down
+	//  Step 5 - E up with CoG and CoM
+	//  Step 6 - E down
+	//  Step 7 - E up with RuM
+	//  Step 8 - E up with PoG and PoM
+	//  Step 9 - E up with PoG and PoM
+	//  Step 10 - E down
+	//  Step 11 - E down
+	//   (repeat 8-11 for Pr/Th/Ru/Co) which is another 14 steps (ignore the last 2 downs)
+	// (last step was 24)
+	//  
+	// Ok that's wrong. not that easy to by hand!  come back to this later i think  
+	
+	
+	
 	@Override
 	public void run(String input, int part, Output out) {
 		if (part == 0) {
--- src/uk/co/stikman/aoc/year2016/Day12.java
+++ src/uk/co/stikman/aoc/year2016/Day12.java
@@ -1,5 +1,9 @@
 package uk.co.stikman.aoc.year2016;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import uk.co.stikman.aoc.utils.AoCBase;
 import uk.co.stikman.aoc.utils.ConsoleOutput;
 import uk.co.stikman.aoc.utils.Output;
@@ -12,11 +16,44 @@ public class Day12 extends AoCBase {
 
 	@Override
 	public void run(String input, int part, Output out) {
-		if (part == 0) {
-		} else if (part == 1) {
-		} else
-			throw new RuntimeException("Part.. ?");
+		List<String> lines = Arrays.stream(input.split("\n")).map(String::trim).collect(Collectors.toList());
+			// 
+			// instructions are CPY n/r r, INC r, DEC r, JMP n/r n/r
+			//
+			int[] registers = new int[4];
+			if (part == 1)
+				registers[2] = 1; // init c to 1
+			
+			int ptr = 0;
+			while (ptr < lines.size()) {
+				String[] bits = lines.get(ptr++).split(" ");
+				if ("cpy".equals(bits[0])) {
+					registers[bits[2].charAt(0) - 'a'] = getOperand(bits[1], registers);
+				} else if ("inc".equals(bits[0])) {
+					registers[bits[1].charAt(0) - 'a']++;
+				} else if ("dec".equals(bits[0])) {
+					registers[bits[1].charAt(0) - 'a']--;
+				} else if ("jnz".equals(bits[0])) {
+					//
+					// if first bit isn't zero then it jumps to relative bits[2]
+					//
+					int one = getOperand(bits[1], registers);
+					if (one != 0) {
+						int two = getOperand(bits[2], registers);
+						--ptr; // we've alreayd offset this so wind it back
+						ptr += two;
+					}
+				}
+			}
+			out.println("Part " + part + " register contents: " + Arrays.toString(registers));
 
+	}
+
+	private static final int getOperand(String s, int[] registers) {
+		if (Character.isDigit(s.charAt(0)) || s.charAt(0) == '-')
+			return Integer.parseInt(s);
+		else
+			return registers[s.charAt(0) - 'a'];
 	}
 
 }
