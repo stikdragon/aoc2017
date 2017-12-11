e9331a6cfe774b6cab59d8457aeebc261a8e9e33
Stik <stikdragon@gmail.com>
2017-12-11 21:12:22 +0000

    day 11

--- src/uk/co/stikman/aoc/utils/AStar.java
+++ src/uk/co/stikman/aoc/utils/AStar.java
@@ -3,7 +3,6 @@ package uk.co.stikman.aoc.utils;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -24,22 +23,23 @@ public abstract class AStar<T> {
 	 */
 	public List<T> execute(T start, T destination) {
 
-		Set<T> open = new HashSet<>();
+		final FloatMap<T> gscores = new FloatMap<>(Integer.MAX_VALUE);
+		final FloatMap<T> fscores = new FloatMap<>(Integer.MAX_VALUE);
+		SortableSet<T> open = new SortableSet<T>((a, b) -> {
+			return Float.compare(fscores.get(a), fscores.get(b));
+		});
 		Set<T> closed = new HashSet<>();
 		Map<T, T> from = new HashMap<>();
-		IntMap<T> gscores = new IntMap<>(Integer.MAX_VALUE);
 		gscores.put(start, 0);
-		IntMap<T> fscores = new IntMap<>(Integer.MAX_VALUE);
 		fscores.put(start, costEstimate(start, destination));
+
 
 		List<T> tmp = new ArrayList<>();
 		open.add(start);
 		while (!open.isEmpty()) {
-			Iterator<T> iter = open.iterator();
-			T cur = iter.next();
-			iter.remove();
+			T cur = open.removeFirst();
 
-			if (cur.equals(destination))
+			if (cur.equals(destination)) 
 				return constructPath(from, cur);
 
 			closed.add(cur);
@@ -52,7 +52,7 @@ public abstract class AStar<T> {
 					continue;
 				if (!open.contains(neigh))
 					open.add(neigh);
-				int tent = gscores.get(cur) + dist(cur, neigh);
+				float tent = gscores.get(cur) + dist(cur, neigh);
 				if (tent >= gscores.get(neigh))
 					continue;
 				from.put(neigh, cur);
@@ -66,9 +66,9 @@ public abstract class AStar<T> {
 
 	protected abstract void getNeighbours(T cur, List<T> list);
 
-	protected abstract int dist(T from, T to);
+	protected abstract float dist(T from, T to);
 
-	protected abstract int costEstimate(T from, T to);
+	protected abstract float costEstimate(T from, T to);
 
 	private List<T> constructPath(Map<T, T> from, T cur) {
 		List<T> res = new ArrayList<>();
--- src/uk/co/stikman/aoc/year2017/Day11.java
+++ src/uk/co/stikman/aoc/year2017/Day11.java
@@ -6,80 +6,59 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import uk.co.stikman.aoc.maths.Matrix3;
 import uk.co.stikman.aoc.maths.Vector2;
-import uk.co.stikman.aoc.utils.AStar;
 import uk.co.stikman.aoc.utils.AoCBase;
 import uk.co.stikman.aoc.utils.ConsoleOutput;
-import uk.co.stikman.aoc.utils.Coord;
 import uk.co.stikman.aoc.utils.Output;
 
 public class Day11 extends AoCBase {
-
+	
 	public static void main(String[] args) {
 		new Day11().run(SourceData.get(11), 0, new ConsoleOutput());
 		new Day11().run(SourceData.get(11), 1, new ConsoleOutput());
 	}
 
+	//
+	// Gah, so i started doing A* on this before realising how much simpler
+	// it was
+	//
+	
 	@Override
 	public void run(String input, int part, Output out) {
 		List<String> moves = Arrays.stream(input.split(",")).map(String::trim).collect(Collectors.toList());
 
 		Map<String, Vector2> dirs = new HashMap<>();
+		dirs.put("n", new Vector2(0, -1));
 		dirs.put("ne", new Vector2(1, -1));
-		dirs.put("se", new Vector2(1, 1));
+		dirs.put("se", new Vector2(1, 0));
 		dirs.put("s", new Vector2(0, 1));
 		dirs.put("sw", new Vector2(-1, 1));
-		dirs.put("nw", new Vector2(-1, 1));
-		dirs.put("n", new Vector2(0, -1));
+		dirs.put("nw", new Vector2(-1, 0));
 
+		Vector2 start = Vector2.ZERO;
 		Vector2 cur = new Vector2();
+		int bestN = 0;
 		for (String move : moves) {
 			Vector2 dir = dirs.get(move);
 			cur.addLocal(dir);
+			if (stepsBetween(start, cur) > bestN)
+				bestN = stepsBetween(start, cur);
 		}
-		
-		final Matrix3 skew = new Matrix3();
-		skew.skew(1.0f, 1.5f);
-		final Matrix3 iskew = skew.inverse();
-		final Vector2 v1 = new Vector2();
-		final Vector2 v2 = new Vector2();
-		final Vector2 v3 = new Vector2();
-		final Vector2 v4 = new Vector2();
-		//
-		// Do search to find route
-		//
-		AStar<Vector2> star = new AStar<Vector2>() {
-			@Override
-			protected void getNeighbours(Vector2 cur, List<Vector2> list) {
-				for (Vector2 v : dirs.values())
-					list.add(cur.add(v, new Vector2()));
-			}
 
-			@Override
-			protected int dist(Vector2 from, Vector2 to) {
-				skew.multiply(from, v1);
-				skew.multiply(to, v2);
-				return (int) v1.distanceTo(v2);
-			}
+		Vector2 finish = cur;
+		int steps = stepsBetween(start, finish);
 
-			@Override
-			protected int costEstimate(Vector2 from, Vector2 to) {
-				skew.multiply(from, v1);
-				skew.multiply(to, v2);
-				return (int) v1.distanceTo(v2);
-			}
-		};
-		
-
-		out.println("Part 1: Position = " + cur);
-		List<Vector2> path = star.execute(Vector2.ZERO, cur);
-		out.println("Part 1: parth length = " + path.size());
+		if (part == 0) {
+			out.println("Part 1: Position = " + cur);
+			out.println("Part 1: Steps = " + steps);
+		}
+		if (part == 1) 
+			out.println("Part 2: greatest distance is: " + bestN);
 
 	}
 
-	private int hexDist(Vector2 start, Vector2 end) {
-		return 0;
+	private int stepsBetween(Vector2 start, Vector2 finish) {
+		return (int) (Math.abs(finish.y - start.y) + Math.abs(finish.x - start.x));
 	}
 
 }
